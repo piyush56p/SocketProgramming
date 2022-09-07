159ebf3349dd6e5293030c5d7b74a6349e6f8e1e
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-09-07 23:58:32 +0500

    Update serverudp.c

--- serverudp.c
+++ serverudp.c
@@ -5,31 +5,31 @@
 #include <netdb.h>
 #include <unistd.h>
 int main(){
-	char sendline[100];
-	char revline[100];
-	char *hello = "Hello from the server";
-	int sock_fd;
-	int n;
-	struct sockaddr_in servaddr, clientaddr;
-	
-	bzero(&servaddr, sizeof(servaddr));
-	bzero(&clientaddr, sizeof(clientaddr));
-	
-	sock_fd = socket(AF_INET,SOCK_DGRAM,0);
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons (22000);
-	
-	bind(sock_fd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-	int len;
-	len = sizeof(clientaddr);
-	while(1){
-	n = recvfrom(sock_fd, revline,100,0, (struct sockaddr*)&clientaddr, &len);
-	
-	revline[n] = '\0';
-	printf("cleint %s", revline);
-	fgets(sendline,100,stdin);
-	sendto(sock_fd,sendline, strlen(sendline), 0, (const struct sockaddr*) &clientaddr, sizeof(clientaddr));
-	}
-	return 0;
+    char sendline[100];
+    char revline[100];
+    
+    int sock_fd;
+    int n;
+    struct sockaddr_in servaddr, clientaddr;
+    
+    bzero(&servaddr, sizeof(servaddr));
+    bzero(&clientaddr, sizeof(clientaddr));
+    
+    sock_fd = socket(AF_INET,SOCK_DGRAM,0);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons (22000);
+    
+    bind(sock_fd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    int len;
+    len = sizeof(clientaddr);
+    while(1){
+    recvfrom(sock_fd, revline,100,0, (struct sockaddr*)&clientaddr, &len);
+    
+    
+    printf("%s", revline);
+    fgets(sendline,100,stdin);
+    sendto(sock_fd,sendline, strlen(sendline), 0, (struct sockaddr*) &clientaddr, sizeof(clientaddr));
+    }
+    return 0;
 }
